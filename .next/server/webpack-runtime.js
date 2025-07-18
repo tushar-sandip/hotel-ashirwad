b6ca7b0358bf4d3e88d03deebff4a9be8599f15f
Tushar Phalke <tusharphalke@Tushars-MacBook-Air.local>
2025-07-18 23:58:44 +0530

    room detials changes added

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9efc24b40535537b")
+/******/ 		__webpack_require__.h = () => ("55aa88ee62b1a756")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- components/layout/Breadcrumb.js
+++ components/layout/Breadcrumb.js
@@ -3,22 +3,20 @@ import Link from "next/link"
 export default function Breadcrumb({ breadcrumbTitle }) {
     return (
         <>
-            
-            
             <div className="page-title" style={{ backgroundImage: "url(assets/images/background/page-title.jpg)" }}>
                 <div className="auto-container">
-                    <h1>{breadcrumbTitle}</h1>
+                    <h1>{breadcrumbTitle || "Rooms"}</h1>
                 </div>
             </div>
             <div className="bredcrumb-wrap">
                 <div className="auto-container">
                     <ul className="bredcrumb-list">
                         <li><Link href="/">Home</Link></li>
-                        <li>{breadcrumbTitle}</li>
+                        <li><Link href="/room">Rooms</Link></li>
+                        {breadcrumbTitle && <li>{breadcrumbTitle}</li>}
                     </ul>
                 </div>
             </div>
-
         </>
     )
 }
